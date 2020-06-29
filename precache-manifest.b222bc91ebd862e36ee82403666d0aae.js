8a8728bf6ce05bac075b76946c1fe24ed73ee1ae
Renata Ernestina <renatarene.silva573@gmail.com>
2020-06-28 23:42:39 -0300

    Updates

---
+++ precache-manifest.b222bc91ebd862e36ee82403666d0aae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db710b63bf9ee441773130bbd983a4c2",
+    "url": "/react-juros-compostos/index.html"
+  },
+  {
+    "revision": "adeed440759d2db7bcc3",
+    "url": "/react-juros-compostos/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "6c9f6aa5736b2d465903",
+    "url": "/react-juros-compostos/static/css/main.464df1a8.chunk.css"
+  },
+  {
+    "revision": "adeed440759d2db7bcc3",
+    "url": "/react-juros-compostos/static/js/2.b37c9c75.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/react-juros-compostos/static/js/2.b37c9c75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c9f6aa5736b2d465903",
+    "url": "/react-juros-compostos/static/js/main.1b6d702e.chunk.js"
+  },
+  {
+    "revision": "cc1a3a919e4bc23cf85e",
+    "url": "/react-juros-compostos/static/js/runtime-main.6f0f9a15.js"
+  }
+]);
